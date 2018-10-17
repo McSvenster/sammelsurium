9e85ac623cfcc5d2c1ce6b31f025420514967b7a
kosven <sven.koesling@library.ethz.ch>
2018-10-17 16:51:45 +0200

    Skript funktioniert Nach Anforderung von e65

--- seq2csv.py
+++ seq2csv.py
@@ -8,21 +8,26 @@ import re
 
 inputfile = sys.argv[1]
 
-fields = {"245 ": "$$a", "100 ": "$$a", "264 ": "$$c", "PST4": "$$j"}
+fields = {"245": "$$a", "100": "$$a", "260": "$$c", "PST": "$$j"}
 
-sysno = ""
+outputfile = open("ausgabe.csv", "w")
+
+rec = { "100": "Autor", "245": "Titel", "260": "Publikationsdatum", "PST": "Signatur", "sysno": "Systemnummer"}
+
 with open(inputfile, 'r') as sequa:
     for line in sequa:
         '''zeilenweises Abarbeiten des sequential-files'''
-        if (line[:9] != sysno):
-            # print("Record", sysno, "wird in csv geschrieben.")
-            sysno = line[:9]
-            rec = {}
+        if (line[:9] != rec["sysno"]):
+            # print("Record", rec["sysno"], "wird in csv geschrieben.")
+            output = "\";\"".join([rec["100"], rec["245"], rec["260"], rec["PST"], rec["sysno"]])
+            outputfile.write("\"" + output + "\"\n")
+            rec = {"100": "", "245": "", "260": "", "PST": "", "sysno": line[:9]}
 
-        if (line[10:14] in fields.keys() and fields[line[10:14]] in line):
+        if (line[10:13] in fields.keys() and fields[line[10:13]] in line):
             try:
-                content = re.search(re.escape(fields[line[10:14]]) + '(.+?)\$\$', line).group(1)
+                content = re.search(re.escape(fields[line[10:13]]) + '(.+?)\$\$', line).group(1)
             except AttributeError:
-                content = re.search(re.escape(fields[line[10:14]]) + '(.+?)$', line).group(1)
-            rec[line[10:14]] = content
-            print(line[10:14],"-->",content)
+                content = re.search(re.escape(fields[line[10:13]]) + '(.+?)$', line).group(1)
+            rec[line[10:13]] = content
+
+outputfile.close
